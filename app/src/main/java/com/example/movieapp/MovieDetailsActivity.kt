cddde2af323a13dad491808e9ff1d13d5d823789
osualaFC <fredrick.osuala@decagon.dev>
2020-10-16 20:14:42 +0100

    setup retrofit client

--- app/src/main/java/com/example/movieapp/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/movieapp/MovieDetailsActivity.kt
@@ -9,3 +9,7 @@ class MovieDetailsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_movie_details)
     }
 }
+
+//api request--https://api.themoviedb.org/3/movie/550?api_key=9d6c64427ab395f6f6eb05b250e4850f
+//api-key--9d6c64427ab395f6f6eb05b250e4850f
+//api RAT--eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZDZjNjQ0MjdhYjM5NWY2ZjZlYjA1YjI1MGU0ODUwZiIsInN1YiI6IjVmODllNDVmMzk1NDlhMDAzOGJhNzRiOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OKiIHrVh5rttovzGGicbqXazNm6a-dMn5vWns92WhM0
